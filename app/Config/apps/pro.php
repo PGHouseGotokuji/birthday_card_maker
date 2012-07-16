3e22ec5fbb0a8289b6c501fd5f04c362613b27ec
Hiroki Masui <gm1985masui@gmail.com>
2012-07-16 09:02:38 +0900

    本番環境設定読み込み追加

--- app/Config/apps/pro.php
+++ app/Config/apps/pro.php
@@ -1,2 +1,11 @@
 <?php
+/*
+ * dev環境用の設定
+ */
 
+/** 開発環境URL **/
+define('SITE_URL', 'http://birthday-card-maker.pgh-gotokuji.net/');
+
+/** Facebook **/
+define('APP_ID', '246022865501334');
+define('APP_SECRET', 'deddce329f77a2b2fbaa7e17b4584dcd');
