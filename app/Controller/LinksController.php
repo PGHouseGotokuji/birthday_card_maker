581f064c77ac971ce528bf391a4e53e9ad82af2b
Hiroki Masui <gm1985masui@gmail.com>
2012-07-16 13:48:11 +0900

    参加者のログイン機能作成

--- app/Controller/LinksController.php
+++ app/Controller/LinksController.php
@@ -21,7 +21,13 @@ class LinksController extends AppController {
         if (empty($_GET['code'])) {
             $state = sha1(uniqid(mt_rand(), true));
             $this->Session->write('fblogin.state', $state);
-            $this->Session->write('fblogin.ref', '/mypage');
+            $redirectUrl = $this->Session->read('redirectUrl');
+            if (!empty($redirectUrl)) {
+                $this->Session->delete('redirectUrl');
+                $this->Session->write('fblogin.ref', $redirectUrl);
+            } else {
+                $this->Session->write('fblogin.ref', '/mypage');
+            }
             $params = array(
                 'client_id'    => APP_ID,
                 'redirect_uri' => SITE_URL . '/fblogin',
