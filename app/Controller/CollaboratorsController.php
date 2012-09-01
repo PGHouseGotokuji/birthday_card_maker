07ec946c43b648bd515e83059eaa26d7aac4f0ab
Hiroki Masui <gm1985masui@gmail.com>
2012-09-01 15:09:24 +0900

    細かな修正

--- app/Controller/CollaboratorsController.php
+++ app/Controller/CollaboratorsController.php
@@ -177,7 +177,7 @@ class CollaboratorsController extends AppController
                 $this->Session->setFlash('画像保存時に問題が発生しました。再度お試しください。', 'flash' . DS . 'error');
                 return new CakeResponse(array('body' => json_encode(false)));
             }
-            $this->Session->setFlash('プラン画像を保存しました。', 'flash' . DS . 'success');
+            $this->Session->setFlash('画像を提供していただきありがとうござました。', 'flash' . DS . 'success');
             return new CakeResponse(array('body' => json_encode(true)));
         }
 
